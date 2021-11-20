f94675f9e8820121c22303fefe1062709f8e6028
xovest <79008917+xovest@users.noreply.github.com>
2021-11-20 17:50:20 +0200

    frontend

---
+++ client/src/Chat.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
+
+const Chat = () => {
+  return (
+    <div>sup this is chat</div>
+  )
+};
+
+export default () => (
+  <ApolloProvider client={client}>
+    <Chat />
+  </ApolloProvider>
+);
